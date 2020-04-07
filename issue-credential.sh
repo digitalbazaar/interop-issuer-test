986af795887a481c0606e2dcf76b8f873b52d447
Matthew Collier <mcollier@digitalbazaar.com>
2020-04-07 13:14:05 -0400

    Generate unique credential IDs.

--- issue-credential.sh
+++ issue-credential.sh
@@ -6,7 +6,10 @@ if [ -z "$1" ]; then
 fi
 HOST=$1
 TOKEN=$2
-curl -k -v -d @./mock-credential.json -H 'Content-Type: application/json' \
+UUID=$(uuidgen)
+CREDENTIAL=$(<./mock-credential.json)
+curl -k -v -d "${CREDENTIAL/_UUID_/$UUID}" \
+  -H 'Content-Type: application/json' \
   -H 'Accept: application/json' \
   -H "Authorization: Bearer $TOKEN" \
   https://"$HOST"/vc-issuer/issue | json_pp
